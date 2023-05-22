8934bea1467c1e9f9ddd48194a0cdadfaec3edf5
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2023-05-22 23:31:12 +0530

    PostMapping

--- src/main/java/com/example/simpleroot/controller/UserController.java
+++ src/main/java/com/example/simpleroot/controller/UserController.java
@@ -1,9 +1,6 @@
 package com.example.simpleroot.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/v1/user")
@@ -13,5 +10,10 @@ public class UserController {
     @GetMapping("/getUser")
     public String getUser(){
         return "Simple-Root";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser(){
+        return "User Save !";
     }
 }
