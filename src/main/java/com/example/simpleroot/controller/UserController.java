291dd031fa2ad404c381a36c38c7673cc2e5d5e5
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2023-05-22 23:18:35 +0530

    GetMapping

--- src/main/java/com/example/simpleroot/controller/UserController.java
+++ src/main/java/com/example/simpleroot/controller/UserController.java
@@ -6,7 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "api/v/user")
+@RequestMapping(value = "api/v1/user")
 @CrossOrigin
 public class UserController {
+
+    @GetMapping("/getUser")
+    public String getUser(){
+        return "Simple-Root";
+    }
 }
